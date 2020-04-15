32aeed552b3e7025baa2f473789bd91ea98e4e66
Justin Boggs <jboggs1@gmail.com>
2020-04-14 23:03:04 -0500

    updates

--- Justin/static/js/line.js
+++ Justin/static/js/line.js
@@ -2,4 +2,14 @@ d3.csv("data/merged_data_modified.csv").then(function(covidData) {
 
     console.log(covidData);
 
+    var trace1 = {
+        x: [covidData.days_gt_100],
+        y: [covidData.cases],
+        type: 'scatter'
+    };
+
+    var data = [trace1];
+
+    Plotly.newPlot('line', data);
+
 });
