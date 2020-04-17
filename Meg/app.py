98dce09fe3ff5073835df9bfdba629128442f747
Meg Peterson <54789650+MargaretPeterson@users.noreply.github.com>
2020-04-17 17:40:30 -0500

    Updated app.py

--- Meg/app.py
+++ Meg/app.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import numpy as np
-#import pandas as pd
+import pandas as pd
+import json
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -16,17 +17,6 @@ from flask import Flask, render_template
 #################################################
 engine = create_engine("sqlite:///Country_Stats.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save references to each table
-Merged_Data_Modified = Base.classes.merged_data_modified
-
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -37,14 +27,58 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+df= pd.read_sql_query("select * from 'countries'",con=engine)
 
 @app.route("/")
-def home():
-    results = session.query(Data.Merged_Data_Modified).all()
+def welcome():
 
-    # Unravel results into a 1D array and convert to a list
-    country_data = list(np.ravel(results))
-    return jsonify(country_data)
+    #Returns text on page for all routes
+    return (
+        f"Welcome to the Corona Virus API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/all_data<br/>"
+        f"/api/v1.0/Countries<br/>"
+        f"/api/v1.0/HighIndividualism<br/>"
+        f"/api/v1.0/LowIndividualism")
+
+
+@app.route("/api/v1.0/all_data")
+def all_data():
+
+    results = df.to_html()
+
+    return (results)
+
+    
+@app.route("/api/v1.0/Countries")
+def countries():
+
+    results = pd.read_sql("select * from 'countries'", con=engine)
+
+    #Unravel results into a 1D array and convert to a list
+    results = results.to_json(orient = "table")
+    return jsonify(json.loads(results))
+
+
+@app.route("/api/v1.0/HighIndividualism")
+def idv():
+    
+    #Query specific data
+    df_idv = pd.read_sql_query(f'''select * from countries where countries."idv" > 75 ''',con=engine)
+    
+    results = df_idv.to_html()
+
+    return (results)
+
+@app.route("/api/v1.0/LowIndividualism")
+def col():
+    
+    #Query specific data
+    df_col = pd.read_sql_query(f'''select * from countries where countries."idv" < 25 ''',con=engine)
+    
+    results = df_col.to_html()
+
+    return (results)
 
 
 if __name__ == '__main__':
